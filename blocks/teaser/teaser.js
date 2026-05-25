82f19c21e28d8517326763eadb17280047b79c42
Singh <2353887@cognizant.com>
2026-05-25 18:25:30 +0530

    Done with error handling

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,17 +1,16 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-    [...block.children].forEach((row) => {
-      [...row.children].forEach((div) => {
-        if(div.children.length == 1){
-            div.className = "teaser-picture";
-        }
-        if(div.children.length > 1){
-            div.className = "teaser-text";
-            div.firstElementChild.className = "teaser-heading";
-            div.querySelector('p').className = "teaser-description";
-        }
-      });
-
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((div) => {
+      if (div.children.length === 1) {
+        div.className = 'teaser-picture';
+      }
+      if (div.children.length > 1) {
+        div.className = 'teaser-text';
+        div.firstElementChild.className = 'teaser-heading';
+        div.querySelector('p').className = 'teaser-description';
+      }
     });
-  }
+  });
+}
