d16faf4f932829e0281c574cd57ea009a6a0810e
Singh <2353887@cognizant.com>
2026-05-25 18:15:35 +0530

    removed commits

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -2,14 +2,11 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
     [...block.children].forEach((row) => {
-        console.log("Reached 1");
       [...row.children].forEach((div) => {
         if(div.children.length == 1){
-            console.log("Reached 2");
             div.className = "teaser-picture";
         }
         if(div.children.length > 1){
-            console.log("Reached 3");
             div.className = "teaser-text";
             div.firstElementChild.className = "teaser-heading";
             div.querySelector('p').className = "teaser-description";
